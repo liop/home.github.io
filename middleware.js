a5f8db1dad6a4fe6db7a8314531bc9b559c330a4
liop <tmdqq01@gmail.com>
2024-09-05 12:44:14 +0800

    fix api

--- middleware.js
+++ middleware.js
@@ -3,10 +3,8 @@
  * @Description: 功能：
  */
 import { next } from '@vercel/edge';
-import { getImgUrls } from './functions/bing.mjs';
 
 export default async function middleware(req) {
-  const imgs = await getImgUrls();
   return next({
     headers: {
       'Referrer-Policy': 'origin-when-cross-origin',
